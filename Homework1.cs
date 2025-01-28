5465554f9f1bf8d7f41083829c162b27718e4d36
jaciguy <jaci.guy@gmail.com>
2025-01-27 19:33:53 -0600

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            double X = 2.5;
+            double Y = 3.3;
+            double Z = 4 * X * X + 3 * Y;
+
+            Console.WriteLine($"The value of Z for X = {X} and Y = {Y} is: {Z}");
+        }
+    }
+}
